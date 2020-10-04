2e445e8900eb18a5e51a695117fcf792baf0ee21
Revol32 <72274775+Revol32@users.noreply.github.com>
2020-10-05 03:19:19 +0400

    +не работает регистрация

--- src/TZ1/RunnerAPI.java
+++ src/TZ1/RunnerAPI.java
@@ -8,14 +8,41 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class RunnerAPI {
 
 
-    private final String url = "https://test.uxcrowd.ru:443/api/authentication";
+    private final String urlAuh = "https://test.uxcrowd.ru:443/api/authentication";
+    private final String urlReg = "https://test.uxcrowd.ru:443/api/register";
 
 
-    @Test
+
+
+    public String genMail () {
+        StringBuilder genMail = new StringBuilder();
+        genMail.append(genString(8)).append('@').append(genString(6)).append(".ru");
+        System.out.println(genMail.toString());
+        return genMail.toString();
+    }
+
+
+    public String genString(int length) {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = length;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        System.out.println(generatedString);
+        return generatedString;
+    }
+
+
     public void HttpTest () throws IOException {
 
         Map httpHead = new HashMap<String,String>();
@@ -28,14 +55,39 @@ public class RunnerAPI {
         request.put("submit","Login");
 
 
-        HttpResponse<JsonNode> response = Unirest.post(url).headers(httpHead).cookie("XSRF-TOKEN", "fae6792d-cc98-4b13-a78f-9edffebd26e4").fields(request).asJson();
+        HttpResponse<JsonNode> response = Unirest.post(urlAuh).headers(httpHead)
+                .cookie("XSRF-TOKEN", "fae6792d-cc98-4b13-a78f-9edffebd26e4")
+                .fields(request)
+                .asJson();
         System.out.println(response.getStatus());
         System.out.println(response.getHeaders());
         System.out.println(response.getCookies());
         System.out.println(response.getBody());
-        System.out.println(response);
-
-
 
     }
+
+    @Test
+    public void RegistrationTest () {
+        Map httpHead = new HashMap<String,String>();
+        httpHead.put("x-xsrf-token","ae4357fc-e554-4150-b386-d1562577ae1a");
+
+
+
+        Map request = new HashMap<String,String>();
+        request.put("email",genMail());
+        request.put("role","ROLE_NEW_TESTER");
+        request.put("tariffType","null");
+
+
+        HttpResponse<JsonNode> response = Unirest.post(urlReg).headers(httpHead)
+                .cookie("XSRF-TOKEN", "ae4357fc-e554-4150-b386-d1562577ae1a")
+                .fields(request)
+                .asJson();
+        System.out.println(response.getStatus());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getCookies());
+        System.out.println(response.getBody());
+
+    }
+
 }
