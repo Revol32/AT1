72c8d45ca554105d40c55ce2e956ae07479755f5
Revol32 <72274775+Revol32@users.noreply.github.com>
2020-10-05 17:45:19 +0400

    +тест созадния ТЕСТА

--- src/TZ1/Run1.java
+++ src/TZ1/Run1.java
@@ -2,8 +2,7 @@ package TZ1;
 
 
 import TZ1.Logintest.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.FindBy;
@@ -14,7 +13,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+
+
 
 public class Run1 {
     static {
@@ -24,7 +26,9 @@ public class Run1 {
     private WebDriver driver;
     public WebDriverWait webDriverWait;
     private final String login = "brizer@inbox.ru";
+    private final String loginCustomer = "brizer@ultra-web.ru";
     private final String passtrue = "7EGDDl";
+    private final String passtrueCustomer = "lp1wT2";
     private final String pass = "123412";
     public String url = "https://test.uxcrowd.ru/";
     @FindBy(xpath = "//*[@data-testid='Login menu button']")
@@ -45,8 +49,36 @@ public class Run1 {
     @FindBy(xpath = "//*[@ng-show='authenticationError']")
     protected WebElement errorMassage;
 
+    @FindBy(xpath = "//*[@data-testid='Create a new test button']")
+    protected WebElement createNewTest;
 
-    public void LoginTry (String pwd){
+    @FindBy(xpath = "//*[@data-testid='Test name input']")
+    protected WebElement nameTestField;
+    @FindBy(xpath = "//*[@data-testid='Address site input']")
+    protected WebElement urlTestField;
+    @FindBy(xpath = "//*[@data-testid='Information textarea']")
+    protected WebElement infoTestField;
+    @FindBy(xpath = "//*[@data-testid='Submit button']")
+    protected WebElement nextOneTestButton;
+
+    @FindBy(xpath = "//*[@data-testid='Segment name input 0']")
+    protected WebElement segmentNameField;
+        @FindBy(xpath = "//*[@data-testid='Users count input']")
+        protected WebElement usersCountField;
+    @FindBy(xpath = "//*[@data-testid='Tasks button']")
+    protected WebElement tasksButton;
+    @FindBy(xpath = "//*[@data-testid='Tasks task question']")
+    protected WebElement tasksQuestionField;
+    @FindBy(xpath = "//*[@data-testid='Tasks submit task']")
+    protected WebElement submitTasksQuestionButton;
+    @FindBy(xpath = "//*[@data-testid='Check button']")
+    protected WebElement checkButton;
+    @FindBy(xpath = "//*[@data-testid='Checkout start button']")
+    protected WebElement startTestButton;
+    @FindBy(xpath = "//*[@class='sc-bxivhb iaxSpn']")
+    protected WebElement returnTestListButton;
+
+    public void LoginTry (String login, String pwd){
         MainPage mainPage = new MainPage(driver);
         PageFactory.initElements(driver, this);
         mainPage.open(url);
@@ -67,17 +99,38 @@ public class Run1 {
         return driver;
     }
 
-    @Test
+
     public void TestLoginSuccess () {
-        LoginTry(passtrue);
+        LoginTry(login, passtrue);
         Assert.assertNotNull(logoutButton, "Облом");
     }
 
-    @Test
+
     public void TestLoginFail () {
 
-        LoginTry(pass);
+        LoginTry(login, pass);
         Assert.assertNotNull(errorMassage, "Облом");
+    }
+
+    @Test
+    public  void TestTask () {
+        int rnd = (int) (Math.random()*100000);
+        String randomTestName = "Test "+rnd;
+        LoginTry(loginCustomer, passtrueCustomer);
+        createNewTest.click();
+        nameTestField.sendKeys(randomTestName);
+        urlTestField.sendKeys("test.uxcrowd.ru");
+        infoTestField.sendKeys("Вводная информация для респондента");
+        nextOneTestButton.click();
+        segmentNameField.sendKeys(randomTestName);
+        usersCountField.sendKeys(Keys.BACK_SPACE,"1");
+        tasksButton.click();
+        tasksQuestionField.sendKeys("Задание");
+        submitTasksQuestionButton.click();
+        checkButton.click();
+        startTestButton.click();
+        returnTestListButton.click();
+        logoutButton.click();
     }
 
 
