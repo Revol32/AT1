e562929fabeb995e2520c6e4cc3488cabaf03226
Revol32 <72274775+Revol32@users.noreply.github.com>
2020-10-06 17:14:57 +0400

    Регистрация работает+смена пароля

--- src/TZ1/RunnerAPI.java
+++ src/TZ1/RunnerAPI.java
@@ -1,32 +1,43 @@
 package TZ1;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kong.unirest.*;
 import kong.unirest.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import static org.testng.Assert.assertEquals;
+
 public class RunnerAPI {
 
-
+    private final String urlToken = "https://test.uxcrowd.ru/api/account";
     private final String urlAuh = "https://test.uxcrowd.ru:443/api/authentication";
     private final String urlReg = "https://test.uxcrowd.ru:443/api/register";
+    private final String urlChange = "https://test.uxcrowd.ru:443/api/account/password/change";
     private final String login = "brizer@inbox.ru";
     private final String loginCustomer = "brizer@ultra-web.ru";
     private final String passtrue = "7EGDDl";
     private final String passtrueCustomer = "lp1wT2";
     private final String pass = "123412";
+    private final String loginChange = "brizer7@gmail.com";
+    private String passChange;
     private HttpResponse<JsonNode> response;
+    private HttpResponse<JsonNode> loginResponse;
 
 
     public String genMail () {
         StringBuilder genMail = new StringBuilder();
         genMail.append(genString(8)).append('@').append(genString(6)).append(".ru");
-        System.out.println(genMail.toString());
+        //System.out.println(genMail.toString());
         return genMail.toString();
     }
 
@@ -41,82 +52,127 @@ public class RunnerAPI {
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
-
-        System.out.println(generatedString);
+        //System.out.println(generatedString);
         return generatedString;
     }
 
+    public String getXSRFToken () {
+        HttpResponse<JsonNode> response = Unirest.get(urlToken).asJson();
+        Cookies mapXZ = response.getCookies();
+        Cookie setCookie = mapXZ.getNamed("XSRF-TOKEN");
+        return setCookie.getValue();
+    }
 
-    public HttpResponse<JsonNode> TryLogin (String login, String pwd) throws IOException {
+    public String retSession (HttpResponse<JsonNode> response) {
+        Cookies responseCookies = response.getCookies();
+        Cookie needCookie = responseCookies.getNamed("JSESSIONID");
+        String session = needCookie.getValue();
+        return session;
+    }
 
-        Map httpHead = new HashMap<String, String>();
-        httpHead.put("x-xsrf-token","fae6792d-cc98-4b13-a78f-9edffebd26e4");
+    public HttpResponse<JsonNode> tryLogin(String login, String pwd,String token) throws IOException {
 
-        Map request = new HashMap<String, String>();
+        Map<String, String> httpHead = new HashMap<>();
+        httpHead.put("x-xsrf-token",token);
+
+        Map<String, Object> request = new HashMap<>();
         request.put("username",login);
-        request.put("password","7EGDDl");
+        request.put("password",pwd);
         request.put("remember-me","undefined");
         request.put("submit","Login");
 
 
         HttpResponse<JsonNode> response = Unirest.post(urlAuh).headers(httpHead)
-                .cookie("XSRF-TOKEN", "fae6792d-cc98-4b13-a78f-9edffebd26e4")
+                .cookie("XSRF-TOKEN", token)
                 .fields(request)
                 .asJson();
-        System.out.println(response.getStatus());
-        System.out.println(response.getHeaders());
-        System.out.println(response.getCookies());
-        System.out.println(response.getBody());
         return response;
 
     }
 
-    public void SuccessLogin() throws IOException {
-        response = TryLogin(login,passtrue);
-        Assert.assertEquals(response.getStatus(), 200);
+    public void successLogin() throws IOException {
+        String token = getXSRFToken();
+        response = tryLogin(login,passtrue,token);
+        assertEquals(response.getStatus(), 200);
     }
 
-    public void FailLogin() throws IOException {
-        TryLogin(login,pass);
+    public void failLogin() throws IOException {
+        String token = getXSRFToken();
+        response = tryLogin(login,pass,token);
         Assert.assertFalse(response.getStatus() == 200, "Test");
     }
 
-    @Test
-    public void RegistrationTest () {
+
+    public void registrationTest() {
+        String token = getXSRFToken();
         Map<String, String> httpHead = new HashMap<>();
         httpHead.put("Accept", "application/json, text/plain, */*");
         httpHead.put("Content-Type","application/json");
-        httpHead.put("x-xsrf-token","eb8111fb-9bb2-4d82-9317-2d717eed4c75");
-
-
-
-        /*Map request = new HashMap<String,String>();
-        request.put("email",genMail());
-        request.put("role","ROLE_NEW_TESTER");
-        request.put("tariffType","null");*/
-
+        httpHead.put("x-xsrf-token",token);
 
 
         JSONObject jsonRequest = new JSONObject();
         jsonRequest.put("email",genMail());
         jsonRequest.put("role","ROLE_NEW_TESTER");
-        jsonRequest.put("tariffType","null");
-        String json2 = jsonRequest.toString();
-        System.out.println(json2);
-        /*HttpRequest reqPost = (HttpRequest) Unirest.post(urlReg).headers(httpHead)
-                .cookie("XSRF-TOKEN", "eb8111fb-9bb2-4d82-9317-2d717eed4c75")
-                .body(jsonRequest);
-       // System.out.println("Pfghjc    :  " +reqPost);*/
+        jsonRequest.put("tariffType", (String) null);
 
         HttpResponse<JsonNode> response = Unirest.post(urlReg).headers(httpHead)
-                .cookie("XSRF-TOKEN", "eb8111fb-9bb2-4d82-9317-2d717eed4c75")
-                .body("{\"email\":\"pvmonazu@vdfyry.ru\",\"role\":\"ROLE_NEW_TESTER\",\"tariffType\":null}")
-                .asJson();
-        System.out.println(response.getStatus());
-        System.out.println(response.getHeaders());
-        System.out.println(response.getCookies());
-        System.out.println(response.getBody());
+                    .cookie("XSRF-TOKEN", token)
+                    .body(jsonRequest)
+                    .asJson();
+        assertEquals(response.getStatus(), 201);
 
     }
+
+    @Test
+    public void ChangePasswordTest () throws IOException {
+
+        String token = getXSRFToken();
+
+        try (FileReader fileReader = new FileReader("C:\\Users\\brizer\\IdeaProjects\\AT1\\src\\TZ1\\Pass.txt")){
+            BufferedReader reader = new BufferedReader(fileReader);
+            passChange = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(passChange);
+
+        loginResponse = tryLogin(loginChange,passChange,token);
+        assertEquals(loginResponse.getStatus(), 200);
+        String session = retSession(loginResponse);
+        String newPass = genString(6)+"D1";
+        System.out.println(newPass);
+
+        Map<String, String> httpHead = new HashMap<>();
+        httpHead.put("Accept", "application/json, text/plain, */*");
+        httpHead.put("Content-Type","application/json");
+        httpHead.put("x-xsrf-token",token);
+
+        JSONObject jsonRequest = new JSONObject();
+        jsonRequest.put("password_one",passChange);
+        jsonRequest.put("password_new",newPass);
+        jsonRequest.put("repeat_password_new", newPass);
+
+        HttpResponse<JsonNode> passChangeResult = Unirest.post(urlChange).headers(httpHead)
+                .cookie("XSRF-TOKEN", token)
+                .cookie("JSESSIONID", session)
+                .body(jsonRequest)
+                .asJson();
+        System.out.println(passChangeResult.getStatus());
+        System.out.println(passChangeResult.getHeaders());
+        System.out.println(passChangeResult.getCookies());
+        System.out.println(passChangeResult.getBody());
+       if (passChangeResult.getStatus() == 200) {
+            FileWriter writer = new FileWriter("C:\\Users\\brizer\\IdeaProjects\\AT1\\src\\TZ1\\Pass.txt", false);
+            writer.write(newPass);
+            writer.close();
+            System.out.println(newPass);
+        }
+
+
+
+    }
+
 
 }
